b0e5ffd0d4717047992452191543ad4b2bb76f40
georgio13 <it11155@uom.edu.gr>
2013-04-30 11:57:25 +0300

    click me

--- board/board/src/PuzzleList.java
+++ board/board/src/PuzzleList.java
@@ -4,14 +4,12 @@ public class PuzzleList {
 	private ArrayList<Puzzle> puzzles;
 		
 	public PuzzleList(){
-		//dhmiourgia listas puzzle prosthiki 1-1 tous grifous
-		puzzles=new ArrayList<Puzzle>();
+		puzzles = new ArrayList<Puzzle>();
 		puzzles.add(new ClickMe());
 		puzzles.add(new AncientArcade());
 		puzzles.add(new TeleCube());
 		puzzles.add(new PicsHeros());
 		puzzles.add(new MemoryGame());
-			
 	}
 
 	public ArrayList<Puzzle> getPuzzles() {
@@ -21,7 +19,4 @@ public class PuzzleList {
 	public void setPuzzles(ArrayList<Puzzle> puzzles) {
 		this.puzzles = puzzles;
 	}
-
-	
-	
 }

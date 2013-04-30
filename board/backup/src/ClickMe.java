2699959fdc5848f0e632095320146107949caccb
georgio13 <it11155@uom.edu.gr>
2013-04-30 23:06:47 +0300

    Click me

--- board/backup/src/ClickMe.java
+++ board/backup/src/ClickMe.java
@@ -1,18 +1,15 @@
-
 public class ClickMe extends Puzzle{
 	private int code;
 	
 	public ClickMe() {
 		super();
-		code=2;
-		
+		code = 2;
 	}
 
 	public void addXP() {
 		//methodos gia prosthiki xp		
 	}
 
-	@Override
 	public void addcoins() {
 		//methodos gia prosthiki coins
 	}
@@ -22,8 +19,6 @@ public class ClickMe extends Puzzle{
 	}
 
 	public void startPuzzle(){
-		//gia na ksekinisei to paixnidi kaloume ton kataskeyasth tou
 		new ClickMeFrame();
-		System.out.println("clickme");
 	}
 }
--- board/backup/src/ClickMeFrame.java
+++ board/backup/src/ClickMeFrame.java
@@ -1,239 +1,189 @@
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
-
+import javax.imageio.ImageIO;
 import javax.swing.*;
-
-import java.util.Timer;
-
-import java.util.TimerTask;
-
-
-
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ClickMeFrame extends JFrame{
-	private TimerBeep timer;
-	private int timerDelay;
-	private JLabel iconLabel;
-	//label gia thn eikona
-	private JLabel backgroundLabel;
-	//label gia to background
-	private JLabel scoreLabel;
-	//label pou emfanizei to score
-	private JLabel lifeLabel;
-	//label pou emfanizei poses prospathies menoun akoma
-	private int iconSize;
-	//arxiko megethos eikonas tha meiwnetai posostiaia analoga me tis epityxies
-	private int score=0;
-	//ayksanetai kata ena meta apo kathe epityxia
-	private int lives=3;
-	//meiwnei kata ena me kathe klik ektos eikonas kai me kathe allagh theshs kata thn opoia den exei mesolabhsei klik
-	private MyGlassPane myGlassPane;
+	private static final long serialVersionUID = 1L;
+	private BackgroundPanel back;
+	private JPanel scorePanel, timePanel;
+	private int iconSize, score, lives, minutes, seconds;
+	private JLabel iconLabel, scoreLabel, lifeLabel, timeLabel;
+	private Image background;
+	private JButton pause;
+	private boolean isRunning;
+	private Timer timer, iconTimer;
+	private TimerClass count;
+	private IconClass help;
 	
 	public ClickMeFrame() {
-		
-		
-		iconSize=150;
-		timerDelay=3000;
-		timer=new TimerBeep(timerDelay);
-		
-		iconLabel=new JLabel();
-		scoreLabel=new JLabel("Score: "+score);
-		lifeLabel=new JLabel("Lives: "+lives);
-		
-		//scoreLabel.setLocation(10,10);
-		
-		ImageIcon bgIcon=new ImageIcon("land.png");
-		backgroundLabel=new JLabel(bgIcon);
-		this.setContentPane(backgroundLabel);
-		
-		myGlassPane=new MyGlassPane();
-		this.setGlassPane(myGlassPane);
-		myGlassPane.setVisible(true);
-		
-		myGlassPane.add(scoreLabel);
-		myGlassPane.add(iconLabel);
-		myGlassPane.add(lifeLabel);
-		//dhmiourgia kai rythmiseis gia to glasspane
-		
-		ImageIcon icon = new ImageIcon("medusa.png");
-		Image image=icon.getImage();
-		//metatroph aytou pou perasame san icon se typo image giati theloume na kanei rezise me thn getScaleInstance
-		//pou einai methodos ths klashs image	
-		
-		
-		iconLabel.setSize(iconSize, iconSize);		
-		//tis diastaseis tha tis baloume me metablhtes gia na allazoun analoga me ta klik
-		//isws xreiastei allh klash h tha mpei sto timer mesa
-	
-		System.out.println(iconLabel.getSize());
-		System.out.println(iconLabel.getWidth());
-		//println gia elegxo
-	
-		Image resizedImage = image.getScaledInstance(iconLabel.getWidth(), iconLabel.getHeight(), 0);
-		//methodos gia na kanei resize h eikona
-		iconLabel.setIcon(new ImageIcon(resizedImage));
-		//afou ginei resize bazoume panw sto label thn kainouria eikona
-    
-		MyListener listener = new MyListener();
-		myGlassPane.addMouseListener(listener);
-		//dhmiourgia kai prosthiki tou listener sto glasspane		
-	
-		this.setSize(500, 500);
-		this.setTitle("Click Me!");
-		this.setResizable(false);
-		this.setVisible(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-	}
-	
-	public void endTime(){
-		if(timer.isExpire()){
-			myGlassPane.repaint();
-			lives=lives-1;
-			if(lives==0){
-				JOptionPane.showMessageDialog(null, "Your Total Score Is: "+score);
-				
-			}
+		minutes =  2;
+		seconds = 0;
+		help = new IconClass();
+		count = new TimerClass(minutes, seconds);
+		iconTimer = new Timer(2000, help);
+		iconTimer.start();
+		timer = new Timer(1000, count);
+		timer.start();
+		isRunning  = true;
+		score = 0;
+		lives = 3;
+		try {
+			background = ImageIO.read(new File("land.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		back = new BackgroundPanel(background);
+		setContentPane(back);
 		
-	}
-	
-	
-	class MyGlassPane extends JComponent{
+		scorePanel = new JPanel();
+		GridBagLayout gbl_scorePanel = new GridBagLayout();
+		gbl_scorePanel.columnWidths = new int[] {40};
+		gbl_scorePanel.rowHeights = new int[] {25};
+		scorePanel.setLayout(gbl_scorePanel);
 		
-		public void paintComponent(Graphics g){
-			//epikalypsh ths repaint
-			super.paintComponent(g);
-			
-			
-			Random r=new Random(System.currentTimeMillis());
-			int randomX=r.nextInt(500-iconSize);
-			int randomY=r.nextInt((480-iconSize)+(score+1));
-			
-			iconLabel.setBounds(randomX,randomY,iconSize,iconSize);
-			
-			
-			// na dw ti ftaiei
-			
-		}
-	}
+		scoreLabel = new JLabel("\u03A3\u03BA\u03BF\u03C1: 0");
+		scoreLabel.setForeground(new Color(128, 0, 0));
+		scoreLabel.setFont(new Font("Sylfaen", Font.BOLD, 20));
+		GridBagConstraints gbc_scoreLabel = new GridBagConstraints();
+		gbc_scoreLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_scoreLabel.gridx = 0;
+		gbc_scoreLabel.gridy = 0;
+		scorePanel.add(scoreLabel, gbc_scoreLabel);
+		back.add(scorePanel, BorderLayout.EAST);
 		
-	class MyListener extends MouseAdapter {		
-		public void mouseClicked(MouseEvent e) {
-			timer.restart();
-			int labelX=iconLabel.getX();
-			int labelY=iconLabel.getY();
-			//pairnoume tis trexouses syntetagmenes X kai Y tou label
-			int clickX=e.getX();
-			int clickY=e.getY();
-			//pairnoume tis syntetagmenes tou klik pou kaname
-			
-			
-			System.out.println(" "+iconLabel.getX()+" "+iconLabel.getY());
-			//ektypwsh elegxou
-			
-			if((clickX>=labelX)&&(clickX<(labelX+iconSize))&&(clickY>=labelY)&&(clickY<(labelY+iconSize))){
-					//elegxos an to klik einai panw sto label
-					score=score+1;
-					//aykshsh score kata ena
-					if ((score%5)==0){
-						System.out.println("A");
-						double size=iconSize/1.05;
-						iconSize=(int)Math.round(size);
-						//meiwsh diastasewn kata 5%
-						//metatroph tou double size se akeraio me stroggylopoihsh gia na mpei sto iconSize
-						double delay=timerDelay/1.20;
-						timerDelay=(int)Math.round(delay);
-						
-						System.out.println("my"+iconSize);
-						iconLabel.setSize(iconSize,iconSize);
-						System.out.println(iconLabel.getWidth()+"");
-						ImageIcon icon = new ImageIcon("medusa.png");
-						Image image=icon.getImage();
-						Image resizedImage = image.getScaledInstance(iconLabel.getWidth(), iconLabel.getHeight(), 0);
-						//methodos gia na kanei resize h eikona
-						iconLabel.setIcon(new ImageIcon(resizedImage));
-						
-						//afou ginei resize bazoume panw sto label thn kainouria eikona
-					}
-					scoreLabel.setText("Score: "+score);
-					//update tou keimenou tou label pou deixnei to score
-					System.out.println("score "+labelX +","+ labelY);
-					//ektypwsh elegxou
-					System.out.println("Hit");
-			}
-			else{
-				System.out.println("Miss");
-				lives=lives-1;
-				if(lives==0){
-					JOptionPane.showMessageDialog(null, "Your Total Score Is: "+score);
-					
+		lifeLabel = new JLabel("\u0396\u03C9\u03AD\u03C2: 3");
+		lifeLabel.setForeground(new Color(128, 0, 0));
+		lifeLabel.setFont(new Font("Sylfaen", Font.BOLD, 20));
+		GridBagConstraints gbc_lifeLabel = new GridBagConstraints();
+		gbc_lifeLabel.gridx = 0;
+		gbc_lifeLabel.gridy = 1;
+		scorePanel.add(lifeLabel, gbc_lifeLabel);
+		
+		timeLabel = new JLabel(minutes+" : 0"+seconds);
+		timeLabel.setFont(new Font("Sylfaen", Font.BOLD, 20));
+		timeLabel.setForeground(new Color(139, 69, 19));
+		timePanel = new JPanel();
+		timePanel.add(timeLabel);
+		pause = new JButton("\u03A0\u03B1\u03CD\u03C3\u03B7");
+		pause.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (isRunning)
+				{
+					isRunning = false;
+					timer.stop();
+					iconTimer.stop();
+					Toolkit.getDefaultToolkit().beep();
+				}
+				else
+				{
+					isRunning = true;
+					timer.start();
+					iconTimer.start();
 				}
 			}
-		System.out.println("score "+labelX +","+ labelY);
-		//ektypwsh elegxou
+		});
+		timePanel.add(pause);
+		back.add(timePanel, BorderLayout.NORTH);
 		
-		myGlassPane.repaint();
-		//kalw thn repaint pou epikalypsa epanw
-		}
-		}
+		iconSize = 150;
+		iconLabel = new JLabel();
+		help.add(iconLabel);
+		ImageIcon icon = new ImageIcon("medusa.png");
+		Image image = icon.getImage();
+		iconLabel.setSize(iconSize, iconSize);		
+		Image resizedImage = image.getScaledInstance(iconLabel.getWidth(), iconLabel.getHeight(), 0);
+		iconLabel.setIcon(new ImageIcon(resizedImage));
+		MyListener listener = new MyListener();
+		help.addMouseListener(listener);
+		back.add(help, BorderLayout.CENTER);
+		
+		this.setVisible(true);
+		this.setSize(500, 500);
+	}
 	
-	class TimerBeep extends Timer{
-		//klash gia ton timer
+	public class IconClass extends JComponent implements ActionListener{
+		private static final long serialVersionUID = 1L;
 
-		private boolean expire=false;
-		  Timer timer;
-		  RemindTask task;
-
-		  public TimerBeep(int seconds) {
-		
-			  
-		    timer = new Timer();
-		  
-		    timer.scheduleAtFixedRate(new RemindTask(),5000,timerDelay);
-		    //xrhsh gia epanalambanomeno gegonos to prwto orisma einai to epanalambanomeno gegonos, to deytero einai
-		    //posh kathysterhsh se milliseconds tha exei mexri thn prwth epanalhpsh apo thn klhsh ths klashs
-		    //to deytero einai to perithorio metaksi dyo epanalhpsewn
-		    
-		    //1 second=1000 miliseconds
-		  }
-
-		  class RemindTask extends TimerTask {
-			  int i=0;
-			  //opoiadhpote energia ginetai bash tou rologiou prepei na kanei extends to TimerTask
-		    public void run() {
-		    	expire=true;
-		      System.out.println("Time's up!"+i);
-		      i++;
-		      endTime();
-		      System.out.println("td"+timerDelay);
-		    
-		       //Stops the AWT thread (and everything else)
-		    }
-		  }
-
-		public void restart() {
-			timer.cancel();
-			//akyrwnei ton prwto timer
-			timer.purge();
-			//akyrwnei oles tis ergasies pou trexoun
-			timer=new Timer();
-			//ftiaxnei neo timer
-			timer.scheduleAtFixedRate(new RemindTask(),timerDelay,timerDelay);
-			
-			//orizei to task tou
-			
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
 		}
 		
+		public void actionPerformed(ActionEvent e) {
+			Random r = new Random(System.currentTimeMillis());
+			int randomX = r.nextInt(400 - iconSize);
+			int randomY = r.nextInt((480 - iconSize) + (score + 1));
+			iconLabel.setBounds(randomX, randomY, iconSize, iconSize);
+		}
+	}
+	
+	public class TimerClass implements ActionListener{
+		int minutes, seconds;
 		
+		public TimerClass(int minutes, int seconds)
+		{
+			this.minutes = minutes;
+			this.seconds = seconds;
+		}
 
-		public boolean isExpire() {
-			//elegxei an exei lhksei o xronos
-			return expire;
+		public void actionPerformed(ActionEvent arg0) {
+			if(seconds == 0)
+			{
+				minutes--;
+				seconds = 59;
+			}
+			else
+			{
+				seconds--;
+			}
+
+			if (seconds < 10)
+			{
+				timeLabel.setText(minutes+" : 0"+seconds);
+			}
+			else
+			{
+				timeLabel.setText(minutes+" : "+seconds);
+			}
+			
+			if (seconds == 0 && minutes == 0)
+			{
+				timer.stop();
+				Toolkit.getDefaultToolkit().beep();
+			}
+		}
+	}
+			
+	class MyListener extends MouseAdapter {		
+		public void mouseClicked(MouseEvent e) 
+		{
+			int labelX = iconLabel.getX();
+			int labelY = iconLabel.getY();
+			int clickX = e.getX();
+			int clickY = e.getY();
+			
+			if((clickX >= labelX) && (clickX < (labelX + iconSize)) && (clickY >= labelY) && (clickY < (labelY + iconSize)))
+			{
+					score = score + 1;
+					scoreLabel.setText("\u03A3\u03BA\u03BF\u03C1: "+score);
+			}
+			else
+			{
+				lives = lives - 1;
+				lifeLabel.setText("\u0396\u03C9\u03AD\u03C2: "+lives);
+				if(lives == 0 || (minutes == 0 && seconds == 0))
+				{
+					ClickMeFrame.this.setVisible(false);
+					JOptionPane.showMessageDialog(null, "\u03A4\u03BF \u03C4\u03B5\u03BB\u03B9\u03BA\u03CC \u03C3\u03BA\u03BF\u03C1 \u03B5\u03AF\u03BD\u03B1\u03B9: "+score);
+				}
+			}
+			help.repaint();
 		}
 	}
 }

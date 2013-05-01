479d4792f461819e11421683e056e23f36c5228a
nansyg21 <it12148@uom.edu.gr>
2013-05-01 15:27:58 +0300

    DuelBoard

--- board/backup/DuelBoardFrame.java
+++ board/backup/DuelBoardFrame.java
@@ -391,7 +391,7 @@ public class DuelBoardFrame extends JFrame {
 
 			int xhDamRec=herolblPanel.getX() + widthSize;
 			int yhDamRec=herolblPanel.getY()+(heightSize+(heightSize/2));
-			int recWidthA=(int)currUser.getDamage();
+			int recWidthA=(int)currUser.getAttack();
 			g.fillRect(xhDamRec, yhDamRec, recWidthA, 10);
 
 			int xoDamRec=opplblPanel.getX() + widthSize;

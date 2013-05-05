4b9b907bd9103f6bf51513b93c177fdfe39b4460
georgio13 <it11155@uom.edu.gr>
2013-05-05 20:26:37 +0300

    uicons

--- board/backup/src/Uicons.java
+++ board/backup/src/Uicons.java
@@ -3,164 +3,162 @@ import javax.swing.*;
 
 public class Uicons extends ImageIcon {
 	private static final long serialVersionUID = 1L;
-	private ArrayList<ImageIcon> picsHerosIcons;
-	private ArrayList<CardLabel> mmg_icons;//Memory Game
-	private ArrayList<ImageIcon> ArcadeIcons;
+	private ArrayList<ImageIcon> picsHerosIcons, mmg_icons, ArcadeIcons;
 	
-	public Uicons(){		
+	public Uicons() {		
 	
 	//3pics1hero icons
-	picsHerosIcons=new ArrayList<ImageIcon>();
-		
-	ImageIcon iconDedalus1=new ImageIcon("UIcons\\picshero_dedalos_labyrinth.jpg");
-	ImageIcon iconDedalus2=new ImageIcon("UIcons\\picshero_dedalos_talos.jpg");
-	ImageIcon iconDedalus3=new ImageIcon("UIcons\\picshero_dedalos.gif");
+	picsHerosIcons = new ArrayList<ImageIcon>();
 	
-	picsHerosIcons.add(iconDedalus1); //0
-	picsHerosIcons.add(iconDedalus2); //1
-	picsHerosIcons.add(iconDedalus3); //2 
+	ImageIcon iconDedalus2 = new ImageIcon("UIcons\\picshero_dedalos_talos.jpg");
+	iconDedalus2.setDescription("Τάλο");
+	ImageIcon iconDedalus3 = new ImageIcon("UIcons\\picshero_dedalos.gif");
+	iconDedalus3.setDescription("Δαίδαλο");
+	ImageIcon iconDionysus1 = new ImageIcon("UIcons\\picshero_dioniysus_Pan.jpg");
+	iconDionysus1.setDescription("Πάνα");
+	ImageIcon iconDionysus3 = new ImageIcon("UIcons\\picshero_dionysus.jpg");
+	iconDionysus3.setDescription("Διόνυσο");
+	ImageIcon iconOdysseas1 = new ImageIcon("UIcons\\picshero_odysseas_doureios.jpg");
+	iconOdysseas1.setDescription("Δούρειο Ίππο");
+	ImageIcon iconOdysseas3 = new ImageIcon("UIcons\\picshero_odysseas_polyfimos.jpg");
+	iconOdysseas3.setDescription("Πολύφημο");
+	ImageIcon iconPerseas3 = new ImageIcon("UIcons\\picshero_perseus_zeus.jpg");
+	iconPerseas3.setDescription("Δία");
+	ImageIcon iconPromitheas3 = new ImageIcon("UIcons\\picshero_promitheas.jpg");
+	iconPromitheas3.setDescription("Προμηθέα");
+	ImageIcon iconThiseas2 = new ImageIcon("UIcons\\picshero_thiseas_minotauros.png");
+	iconThiseas2.setDescription("Μινόταυρο");
+	ImageIcon iconOlympus = new ImageIcon("UIcons\\olympus.jpg");
+	iconOlympus.setDescription("Όλυμπο");
+	picsHerosIcons.add(iconDedalus2);
+	picsHerosIcons.add(iconDedalus3); 
+	picsHerosIcons.add(iconDionysus1); 
+	picsHerosIcons.add(iconDionysus3); 
+	picsHerosIcons.add(iconOdysseas1);
+	picsHerosIcons.add(iconOdysseas3);
+	picsHerosIcons.add(iconPerseas3);
+	picsHerosIcons.add(iconPromitheas3);
+	picsHerosIcons.add(iconThiseas2);
+	picsHerosIcons.add(iconOlympus);
 	
-	ImageIcon iconDionysus1=new ImageIcon("UIcons\\picshero_dioniysus_Pan.jpg");
-	ImageIcon iconDionysus2=new ImageIcon("UIcons\\picshero_dionysus_wine.jpg");
-	ImageIcon iconDionysus3=new ImageIcon("UIcons\\picshero_dionysus.jpg");
-	
-	picsHerosIcons.add(iconDionysus1); //3
-	picsHerosIcons.add(iconDionysus2); //4
-	picsHerosIcons.add(iconDionysus3); //5
-	
-	ImageIcon iconHercules1=new ImageIcon("UIcons\\picshero_Hercules_hydra.png");
-	ImageIcon iconHercules2=new ImageIcon("UIcons\\picshero_Hercules_nemealion.jpg");
-	ImageIcon iconHercules3=new ImageIcon("UIcons\\picshero_Hercules_snake.jpg");
-	
-	picsHerosIcons.add(iconHercules1); //6
-	picsHerosIcons.add(iconHercules2); //7
-	picsHerosIcons.add(iconHercules3); //8
-	
-	ImageIcon iconIaswnas1=new ImageIcon("UIcons\\picshero_iaswnas_argw.jpg");
-	ImageIcon iconIaswnas2=new ImageIcon("UIcons\\picshero_iaswnas_sympligades.jpg");
-	ImageIcon iconIaswnas3=new ImageIcon("UIcons\\picshero_iaswnas_xderas.jpg");
-	
-	picsHerosIcons.add(iconIaswnas1); //9
-	picsHerosIcons.add(iconIaswnas2); //10
-	picsHerosIcons.add(iconIaswnas3); //11
-	
-	ImageIcon iconOdysseas1=new ImageIcon("UIcons\\picshero_odysseas_doureios.jpg");
-	ImageIcon iconOdysseas2=new ImageIcon("UIcons\\picshero_odysseas_kirki.jpg");
-	ImageIcon iconOdysseas3=new ImageIcon("UIcons\\picshero_odysseas_polyfimos.jpg");
-	
-	picsHerosIcons.add(iconOdysseas1); //12
-	picsHerosIcons.add(iconOdysseas2); //13
-	picsHerosIcons.add(iconOdysseas3); //14
-	
-	ImageIcon iconPerseas1=new ImageIcon("UIcons\\picshero_perseus_argos.jpg");
-	ImageIcon iconPerseas2=new ImageIcon("UIcons\\picshero_perseus_medusa.jpg");
-	ImageIcon iconPerseas3=new ImageIcon("UIcons\\picshero_perseus_zeus.jpg");
-	
-	picsHerosIcons.add(iconPerseas1); //15
-	picsHerosIcons.add(iconPerseas2); //16
-	picsHerosIcons.add(iconPerseas3); //17
-	
-	ImageIcon iconPromitheas1=new ImageIcon("UIcons\\picshero_promitheas_caucasus.gif");
-	ImageIcon iconPromitheas2=new ImageIcon("UIcons\\picshero_promitheas_fwtia.png");
-	ImageIcon iconPromitheas3=new ImageIcon("UIcons\\picshero_promitheas.jpg");
-	
-	picsHerosIcons.add(iconPromitheas1); //18
-	picsHerosIcons.add(iconPromitheas2); //19
-	picsHerosIcons.add(iconPromitheas3); //20
-	
-	ImageIcon iconThiseas1=new ImageIcon("UIcons\\picshero_thiseas_gyrismos.jpg");
-	ImageIcon iconThiseas2=new ImageIcon("UIcons\\picshero_thiseas_minotauros.png");
-	ImageIcon iconThiseas3=new ImageIcon("UIcons\\picshero_thiseas_taurosmarathona.jpg");
-	
-	picsHerosIcons.add(iconThiseas1); //21
-	picsHerosIcons.add(iconThiseas2); //22
-	picsHerosIcons.add(iconThiseas3); //23
-	
-	ImageIcon iconOlympus=new ImageIcon("UIcons\\olympus.jpg");
-	picsHerosIcons.add(iconOlympus); //24 background
+	ImageIcon iconHercules1 = new ImageIcon("UIcons\\picshero_Hercules_hydra.png");
+	iconHercules1.setDescription("Ύδρα");
+	ImageIcon iconHercules2 = new ImageIcon("UIcons\\picshero_Hercules_nemealion.jpg");
+	iconHercules2.setDescription("Νεμέα");
+	ImageIcon iconIaswnas1 = new ImageIcon("UIcons\\picshero_iaswnas_argw.jpg");
+	iconIaswnas1.setDescription("Αργώ");
+	ImageIcon iconOdysseas2 = new ImageIcon("UIcons\\picshero_odysseas_kirki.jpg");
+	iconOdysseas2.setDescription("Κίρκη");
+	ImageIcon iconPerseas2 = new ImageIcon("UIcons\\picshero_perseus_medusa.jpg");
+	iconPerseas2.setDescription("Μέδουσα");
+	picsHerosIcons.add(iconHercules1);
+	picsHerosIcons.add(iconHercules2); 
+	picsHerosIcons.add(iconIaswnas1); 
+	picsHerosIcons.add(iconOdysseas2);
+	picsHerosIcons.add(iconPerseas2);
+
+	ImageIcon iconIaswnas3 = new ImageIcon("UIcons\\picshero_iaswnas_xderas.jpg");
+	iconIaswnas3.setDescription("Χρυσόμαλλο Δέρας");
+	ImageIcon iconPerseas1 = new ImageIcon("UIcons\\picshero_perseus_argos.jpg");
+	iconPerseas1.setDescription("Άργος");
+	picsHerosIcons.add(iconIaswnas3); 
+	picsHerosIcons.add(iconPerseas1);
 	
 	//Memory Game Stuff
-	mmg_icons=new ArrayList<CardLabel>();
+	mmg_icons = new ArrayList<ImageIcon>();
 	
-	ImageIcon default_back_mmg=new ImageIcon("UIcons\\mmg_background.jpg"); // 0 backgound
-	ImageIcon default_pattern_mmg=new ImageIcon("UIcons\\mmg_pattern.jpg"); // 1 closed cards (pattern)
-	ImageIcon aphrodite_mmg=new ImageIcon("UIcons\\mmg_aphrodite.jpg");    //  2
-	ImageIcon apollo_mmg=new ImageIcon("UIcons\\mmg_apollo.jpg");           // 3
-	ImageIcon artemis_mmg=new ImageIcon("UIcons\\mmg_artemis.jpg");        //  4
-	ImageIcon athena_mmg=new ImageIcon("UIcons\\mmg_athena.jpg");        //    5
-	ImageIcon hera_mmg=new ImageIcon("UIcons\\mmg_hera.jpg");             //   6
-	ImageIcon pegasus_mmg=new ImageIcon("UIcons\\mmg_pegasus.jpg");       //   7
-	ImageIcon poseidon_mmg=new ImageIcon("UIcons\\mmg_poseidon.jpg");       // 8
-	ImageIcon zeus_mmg=new ImageIcon("UIcons\\mmg_zeus.jpg");               // 9
+	mmg_icons.add(new ImageIcon("UIcons\\mmg_background.jpg"));
+	mmg_icons.add(new ImageIcon("UIcons\\mmg_pattern.jpg"));
 	
-	mmg_icons.add(new CardLabel(default_back_mmg,      "default") );
-	mmg_icons.add(new CardLabel(default_pattern_mmg,  "default2") );
+	ImageIcon aphrodite1 = new ImageIcon("UIcons\\mmg_aphrodite.jpg");
+	ImageIcon aphrodite2 = new ImageIcon("UIcons\\mmg_aphrodite.jpg");
+	aphrodite1.setDescription("1");
+	aphrodite2.setDescription("1");
+	mmg_icons.add(aphrodite1);
+	mmg_icons.add(aphrodite2);
 	
-	mmg_icons.add(new CardLabel(aphrodite_mmg,        "aphrodite") );  //We need twice each icon
-	mmg_icons.add(new CardLabel(aphrodite_mmg,        "aphrodite") );
+	ImageIcon apollo1 = new ImageIcon("UIcons\\mmg_apollo.jpg");
+	ImageIcon apollo2 = new ImageIcon("UIcons\\mmg_apollo.jpg");
+	apollo1.setDescription("2");
+	apollo2.setDescription("2");
+	mmg_icons.add(apollo1);
+	mmg_icons.add(apollo2);
 	
-	mmg_icons.add(new CardLabel(apollo_mmg,              "apollo") );
-	mmg_icons.add(new CardLabel(apollo_mmg,              "apollo") );
-	
-	mmg_icons.add(new CardLabel(artemis_mmg,            "artemis") );	
-	mmg_icons.add(new CardLabel(artemis_mmg,            "artemis") );
+	ImageIcon artemis1 = new ImageIcon("UIcons\\mmg_artemis.jpg");
+	ImageIcon artemis2 = new ImageIcon("UIcons\\mmg_artemis.jpg");
+	artemis1.setDescription("3");
+	artemis2.setDescription("3");
+	mmg_icons.add(artemis1);	
+	mmg_icons.add(artemis2);
 	 
-	mmg_icons.add(new CardLabel(athena_mmg,              "athena") );
-	mmg_icons.add(new CardLabel(athena_mmg,              "athena") );
+	ImageIcon athena1 = new ImageIcon("UIcons\\mmg_athena.jpg");
+	ImageIcon athena2 = new ImageIcon("UIcons\\mmg_athena.jpg");
+	athena1.setDescription("4");
+	athena2.setDescription("4");
+	mmg_icons.add(athena1);
+	mmg_icons.add(athena2);
 	
-	mmg_icons.add(new CardLabel(hera_mmg,                  "hera") );
-	mmg_icons.add(new CardLabel(hera_mmg,                  "hera") );
+	ImageIcon hera1 = new ImageIcon("UIcons\\mmg_hera.jpg");
+	ImageIcon hera2 = new ImageIcon("UIcons\\mmg_hera.jpg");
+	hera1.setDescription("5");
+	hera2.setDescription("5");
+	mmg_icons.add(hera1);
+	mmg_icons.add(hera2);
 	
-	mmg_icons.add(new CardLabel(pegasus_mmg,            "pegasus") );
-	mmg_icons.add(new CardLabel(pegasus_mmg,            "pegasus") );
+	ImageIcon pegasus1 = new ImageIcon("UIcons\\mmg_pegasus.jpg");
+	ImageIcon pegasus2 = new ImageIcon("UIcons\\mmg_pegasus.jpg");
+	pegasus1.setDescription("6");
+	pegasus2.setDescription("6");
+	mmg_icons.add(pegasus1);
+	mmg_icons.add(pegasus2);
 	 
-	mmg_icons.add(new CardLabel(poseidon_mmg,          "poseidon") );
-	mmg_icons.add(new CardLabel(poseidon_mmg,          "poseidon") );
+	ImageIcon poseidon1 = new ImageIcon("UIcons\\mmg_poseidon.jpg");
+	ImageIcon poseidon2 = new ImageIcon("UIcons\\mmg_poseidon.jpg");
+	poseidon1.setDescription("7");
+	poseidon2.setDescription("7");
+	mmg_icons.add(poseidon1);
+	mmg_icons.add(poseidon2);
 	
-	mmg_icons.add(new CardLabel(zeus_mmg,                  "zeus") );
-	mmg_icons.add(new CardLabel(zeus_mmg,                  "zeus") );
-
-	
+	ImageIcon zeus1 = new ImageIcon("UIcons\\mmg_zeus.jpg");
+	ImageIcon zeus2 = new ImageIcon("UIcons\\mmg_zeus.jpg");
+	zeus1.setDescription("8");
+	zeus2.setDescription("8");
+	mmg_icons.add(zeus1);
+	mmg_icons.add(zeus2);
 	
 	//Ancient Arcade Stuff
-		ArcadeIcons=new ArrayList<ImageIcon>();
+	ArcadeIcons = new ArrayList<ImageIcon>();
 		
-		ImageIcon  back_anime=new  ImageIcon("UIcons\\arcade_background.jpg"); //0 background image
+	ImageIcon  back_anime = new  ImageIcon("UIcons\\arcade_background.jpg"); //0 background image
 		
-		ImageIcon  arcade_hero_1=new  ImageIcon("UIcons\\arcade_zeus.jpg"); //1
-		ImageIcon  arcade_hero_2=new  ImageIcon("UIcons\\arcade_poseidon.jpg"); //2 
-		ImageIcon  arcade_hero_3=new  ImageIcon("UIcons\\arcade_hercules.jpg"); //3
-		ImageIcon  arcade_hero_4=new  ImageIcon("UIcons\\arcade_theseus.jpg"); //4
-		ImageIcon  arcade_hero_5=new  ImageIcon("UIcons\\arcade_odysseus.jpg"); //5
-		ImageIcon  arcade_hero_6=new  ImageIcon("UIcons\\arcade_perseus.jpg"); //6
-
-		ImageIcon  arcade_symbol_1=new  ImageIcon("UIcons\\arcade_cerberus.jpg"); //7
-		ImageIcon  arcade_symbol_2=new  ImageIcon("UIcons\\arcade_minotaur.jpg"); //8
-		ImageIcon  arcade_symbol_3=new  ImageIcon("UIcons\\arcade_olympus.jpg"); //9
-		ImageIcon  arcade_symbol_4=new  ImageIcon("UIcons\\arcade_medusa.jpg"); //10
-		ImageIcon  arcade_symbol_5=new  ImageIcon("UIcons\\arcade_scylla.jpg"); //11
-		ImageIcon  arcade_symbol_6=new  ImageIcon("UIcons\\arcade_sea.jpg"); //12
+	ImageIcon  arcade_hero_1 = new  ImageIcon("UIcons\\arcade_zeus.jpg"); //1
+	ImageIcon  arcade_hero_2 = new  ImageIcon("UIcons\\arcade_poseidon.jpg"); //2 
+	ImageIcon  arcade_hero_3 = new  ImageIcon("UIcons\\arcade_hercules.jpg"); //3
+	ImageIcon  arcade_hero_4 = new  ImageIcon("UIcons\\arcade_theseus.jpg"); //4
+	ImageIcon  arcade_hero_5 = new  ImageIcon("UIcons\\arcade_odysseus.jpg"); //5
+	ImageIcon  arcade_hero_6 = new  ImageIcon("UIcons\\arcade_perseus.jpg"); //6
 	
-		
-		//adding the ImageIcons
-		ArcadeIcons.add(back_anime);
-		
-		ArcadeIcons.add(arcade_hero_1);
-		ArcadeIcons.add(arcade_hero_2);
-		ArcadeIcons.add(arcade_hero_3);
-		ArcadeIcons.add(arcade_hero_4);
-		ArcadeIcons.add(arcade_hero_5);
-		ArcadeIcons.add(arcade_hero_6);
-		
-		ArcadeIcons.add(arcade_symbol_1);
-		ArcadeIcons.add(arcade_symbol_2);
-		ArcadeIcons.add(arcade_symbol_3);
-		ArcadeIcons.add(arcade_symbol_4);
-		ArcadeIcons.add(arcade_symbol_5);
-		ArcadeIcons.add(arcade_symbol_6);
-		
+	ImageIcon  arcade_symbol_1 = new  ImageIcon("UIcons\\arcade_cerberus.jpg"); //7
+	ImageIcon  arcade_symbol_2 = new  ImageIcon("UIcons\\arcade_minotaur.jpg"); //8
+	ImageIcon  arcade_symbol_3 = new  ImageIcon("UIcons\\arcade_olympus.jpg"); //9
+	ImageIcon  arcade_symbol_4 = new  ImageIcon("UIcons\\arcade_medusa.jpg"); //10
+	ImageIcon  arcade_symbol_5 = new  ImageIcon("UIcons\\arcade_scylla.jpg"); //11
+	ImageIcon  arcade_symbol_6 = new  ImageIcon("UIcons\\arcade_sea.jpg"); //12	
 	
-	}
+	ArcadeIcons.add(back_anime);
+	ArcadeIcons.add(arcade_hero_1);
+	ArcadeIcons.add(arcade_hero_2);
+	ArcadeIcons.add(arcade_hero_3);
+	ArcadeIcons.add(arcade_hero_4);
+	ArcadeIcons.add(arcade_hero_5);
+	ArcadeIcons.add(arcade_hero_6);
+	ArcadeIcons.add(arcade_symbol_1);
+	ArcadeIcons.add(arcade_symbol_2);
+	ArcadeIcons.add(arcade_symbol_3);
+	ArcadeIcons.add(arcade_symbol_4);
+	ArcadeIcons.add(arcade_symbol_5);
+	ArcadeIcons.add(arcade_symbol_6);
+}
 
 	public ArrayList<ImageIcon> getArcadeIcons() {
 		return ArcadeIcons;
@@ -178,14 +176,11 @@ public class Uicons extends ImageIcon {
 		this.picsHerosIcons = usedIcons;
 	}
 	//Memory Game
-	public ArrayList<CardLabel> getMMGIcons() {
+	public ArrayList<ImageIcon> getMMGIcons() {
 		return mmg_icons;
 	}
 
-	public void setMMGIcons(ArrayList<CardLabel> usedIcons) {
+	public void setMMGIcons(ArrayList<ImageIcon> usedIcons) {
 		this.mmg_icons = usedIcons;
 	}
-	
-	
-	
 }
